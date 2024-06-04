65a4890b09c6d154999c05db5dd9468a8de4aac2
TobyYang7 <tobyyang7@foxmail.com>
2024-06-04 21:06:14 +0800

    add data

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 # to fill in the following path to evaluation!
-output_model=./checkpoints/UrbanGPT
+output_model=./checkpoints_urbangpt/tw2t_multi_reg-cla-gird
 datapath=./data/ST_data_urbangpt/NYC_taxi_cross-region/NYC_taxi.json
 st_data_path=./data/ST_data_urbangpt/NYC_taxi_cross-region/NYC_taxi_pkl.pkl
 res_path=./result_test/cross-region/NYC_taxi
